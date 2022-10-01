a96b645897ce14f1f0b6400751b65b948fc09123
Leonardo Brizolla <leonardo.brizolla@compasso.com.br>
2022-10-01 18:14:16 -0300

    feat(js): to code SaveTodo() [CREATE]

--- js/script.js
+++ js/script.js
@@ -7,15 +7,42 @@ const editInput = document.querySelector("#edit-input");
 const cancelEditBtn = document.querySelector("#cancel-edit-btn");
 
 // FUNCTIONS
+const saveTodo = (text) => {
+  const todo = document.createElement("div");
+  todo.classList.add("todo");
+
+  const todoTitle = document.createElement("h3");
+  todoTitle.innerText = text;
+  todo.appendChild(todoTitle);
+
+  const doneBtn = document.createElement("button");
+  doneBtn.classList.add("finish-todo");
+  doneBtn.innerHTML = '<em class="fa-solid fa-check"></em>';
+  todo.appendChild(doneBtn);
+
+  const editBtn = document.createElement("button");
+  editBtn.classList.add("edit-todo");
+  editBtn.innerHTML = '<em class="fa-solid fa-pen"></em>';
+  todo.appendChild(editBtn);
+
+  const deleteBtn = document.createElement("button");
+  deleteBtn.classList.add("remove-todo");
+  deleteBtn.innerHTML = '<em class="fa-solid fa-xmark"></em>';
+  todo.appendChild(deleteBtn);
+
+  todoList.appendChild(todo);
+
+  todoInput.value = "";
+  todoInput.focus();
+};
+
+// EVENTS
 todoForm.addEventListener("submit", (e) => {
   e.preventDefault();
 
   const inputValue = todoInput.value;
 
   if (inputValue) {
-    // SAVE
-    console.log(inputValue);
+    saveTodo(inputValue);
   }
-})
-
-// EVENTS
+});
